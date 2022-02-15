5eb4ec5b9f32aca240145dd86a6294d4f7cce0b0
Bonetto <barbara.bonetto@gmail.com>
2022-02-15 11:20:39 +0100

    second commit

--- guru/src/main/java/com/giobar/guru/GuruApplication.java
+++ guru/src/main/java/com/giobar/guru/GuruApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GuruApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(GuruApplication.class, args);
 	}
 
